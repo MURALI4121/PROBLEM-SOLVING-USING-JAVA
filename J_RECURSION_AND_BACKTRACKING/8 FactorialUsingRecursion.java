69573c29ffeb5412eb1e165f3a27a021fa447088
MURALI4121 <muralishankar556@gmail.com>
2024-02-21 03:41:34 +0530

    day12

---
+++ J_RECURSION_AND_BACKTRACKING/8 FactorialUsingRecursion.java
@@ -0,0 +1,18 @@
+package J_RECURSION_AND_BACKTRACKING;
+
+class FactorialUsingRecursion {
+    public static void main(String[] args) {
+        int N = 3;
+        System.out.println(factorial(N));
+    }
+
+    public static int factorial(int n){
+
+        // Base case
+        if ( n <= 0){
+            return 1;
+        }
+        //Recursive case
+        return n * factorial(n-1);
+    }
+}
