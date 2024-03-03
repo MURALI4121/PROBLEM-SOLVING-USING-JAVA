534dbf286b5e8c15bef51aae1fde8c511f5d6949
MURALI4121 <muralishankar556@gmail.com>
2024-03-03 23:59:57 +0530

    day22

---
+++ LEETCODE_ARRAYS/MoveZeroes.java
@@ -0,0 +1,33 @@
+package LEETCODE_ARRAYS;
+
+public class MoveZeroes {
+
+  public static void moveZeroes(int[] nums){
+      int j = 0;
+
+      for (int i = 0; i < nums.length; i++) {
+          if(nums[i] !=0 ){
+              nums[j] = nums[i];
+              j++;
+          }
+      }
+      for (int i = j; i < nums.length; i++) {
+          nums[i] = 0;
+      }
+  }
+    public static void main(String[] args) {
+        int[] nums = {0,1,0,3,12};
+
+        System.out.println(" before moving zeroes");
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+
+        System.out.println(" after moving zeroes");
+
+        moveZeroes(nums);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
