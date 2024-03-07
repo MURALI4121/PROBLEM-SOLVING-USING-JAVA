99b1b0ee1bc863b300d1b411a78fe0d50000b22c
MURALI4121 <muralishankar556@gmail.com>
2024-03-07 23:16:02 +0530

    Day24

---
+++ M_BINARY_SEARCH/BinarySearchExample.java
@@ -0,0 +1,32 @@
+package M_BINARY_SEARCH;
+public class BinarySearchExample {
+    public static int binarySearch(int[] arr, int target) {
+
+        int low = 0;
+        int high = arr.length-1;
+
+        while (low <= high){
+            int mid = (low + high) / 2;
+
+            if(arr[mid] == target){
+                return mid; // Target found
+            } else if (arr[mid] < target) {
+                low = mid + 1; // Search in the right half
+            } else {
+                high = mid - 1; // Search in the left half
+            }
+
+        }
+        return -1;
+
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7, 9, 11, 13, 15};
+        int target = 9;
+        int result = binarySearch(arr, target);
+        System.out.println("Index of " + target + " is: " + result);
+
+    }
+}
