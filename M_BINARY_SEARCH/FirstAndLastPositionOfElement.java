e45e9c4375c0f88b14221a803091787a1d77f5f6
MURALI4121 <muralishankar556@gmail.com>
2024-03-10 22:18:19 +0530

    day 27

--- M_BINARY_SEARCH/FirstAndLastPositionOfElement.java
+++ M_BINARY_SEARCH/FirstAndLastPositionOfElement.java
@@ -6,19 +6,70 @@ import java.util.Arrays;
 public class FirstAndLastPositionOfElement {
 
 
-    public static int[] firstAndLastPosition(ArrayList<Integer> arr, int n, int k){
-        int first = -1; int last = -1;
+//    public static int[] firstAndLastPosition(ArrayList<Integer> arr, int n, int k){
+//
+//
+//
+////        int first = -1; int last = -1;
+////
+////        for (int i = 0; i < n-1; i++) {
+////            if(arr.get(i) == k){
+////                if(first == -1){
+////                    first = i ;
+////                }
+////                last = i;
+////            }
+////
+////        }
+////        return new int[] {first , last};
+//
+//
+//    }
+public static int firstOccurrence(ArrayList<Integer> arr, int n, int k) {
+    int low = 0, high = n - 1;
+    int first = -1;
 
-        for (int i = 0; i < n-1; i++) {
-            if(arr.get(i) == k){
-                if(first == -1){
-                    first = i ;
-                }
-                last = i;
-            }
-
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr.get(mid) == k) {
+            first = mid;
+            // look for smaller index on the left
+            high = mid - 1;
+        } else if (arr.get(mid) < k) {
+            low = mid + 1; // look on the right
+        } else {
+            high = mid - 1; // look on the left
         }
-        return new int[] {first , last};
+    }
+    return first;
+}
+
+    public static int lastOccurrence(ArrayList<Integer> arr, int n, int k) {
+        int low = 0, high = n - 1;
+        int last = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // maybe an answer
+            if (arr.get(mid) == k) {
+                last = mid;
+                // look for larger index on the right
+                low = mid + 1;
+            } else if (arr.get(mid) < k) {
+                low = mid + 1; // look on the right
+            } else {
+                high = mid - 1; // look on the left
+            }
+        }
+        return last;
+    }
+
+    public static int[] firstAndLastPosition(ArrayList<Integer> arr, int n, int k) {
+        int first = firstOccurrence(arr, n, k);
+        if (first == -1) return new int[] { -1, -1};
+        int last = lastOccurrence(arr, n, k);
+        return new int[] {first, last};
     }
 
     public static void main(String[] args) {
